fbde3d6a26d3fc5a12ab05dff488a64b19f77745
rcastola <r.castola@gmail.com>
2023-12-21 10:58:10 +0000

    test log in

--- northcoders/projects/group-project-walking-app/rachel_test/y/app.js
+++ northcoders/projects/group-project-walking-app/rachel_test/y/app.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const app = firebase.app();
   console.log(app);
-  console.log("hello");
+  console.log("testing");
 });
 
-function googleLogin() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      const user = result.user;
-      document.write(`Hello ${user.displayName}`);
-      console.log(user);
-    })
-    .catch(console.log);
-}
+// function googleLogin() {
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   firebase
+//     .auth()
+//     .signInWithPopup(provider)
+//     .then((result) => {
+//       const user = result.user;
+//       document.write(`Hello ${user.displayName}`);
+//       console.log(user);
+//     })
+//     .catch(console.log);
+// }
